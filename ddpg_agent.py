da1d374cb8fc168c4752bf7237687b61801ccf2d
Matteo Lucchi <matteo.lucchi@joanneum.at>
2019-07-31 18:14:11 +0200

    feat: updated hyperparameters

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -9,12 +9,12 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 
-BUFFER_SIZE = int(1e5)  # replay buffer size
-BATCH_SIZE = 128        # minibatch size
+BUFFER_SIZE = int(1e6)  # replay buffer size
+BATCH_SIZE = 1024        # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
-LR_ACTOR = 1e-4         # learning rate of the actor 
-LR_CRITIC = 1e-3        # learning rate of the critic
+LR_ACTOR = 1.6e-3         # learning rate of the actor
+LR_CRITIC = 1.1e-3       # learning rate of the critic
 WEIGHT_DECAY = 0        # L2 weight decay
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
