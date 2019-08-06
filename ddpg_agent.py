0b56aafa398d12b2da5103854f7e9826edbdc218
Matteo Lucchi <matteo.lucchi@joanneum.at>
2019-08-06 15:06:52 +0200

    feat: updated hyperparameters to the values of successfull learning

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -13,18 +13,18 @@ BUFFER_SIZE = int(1e6)  # replay buffer size
 BATCH_SIZE = 1024        # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
-LR_ACTOR = 1.6e-3         # learning rate of the actor
-LR_CRITIC = 1.1e-3       # learning rate of the critic
+LR_ACTOR = 1.53e-3         # learning rate of the actor
+LR_CRITIC = 1.03e-3       # learning rate of the critic
 WEIGHT_DECAY = 0        # L2 weight decay
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 class Agent():
     """Interacts with and learns from the environment."""
-    
+
     def __init__(self, state_size, action_size, random_seed):
         """Initialize an Agent object.
-        
+
         Params
         ======
             state_size (int): dimension of each state
